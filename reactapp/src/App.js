2aa0888d8e9aa22250bc734d9627842dbae7b890
ArjanElissen <arjanelissen@hotmail.com>
2021-12-07 11:25:13 +0100

    test1

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,17 @@
 import Header from './componenten/Header';
 import './App.css';
+import React from 'react';
+import Login from './Pages/login.js';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Login />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ reactapp/src/Pages/login.js
@@ -0,0 +1,35 @@
+import React from 'react'; 
+import './login.css';
+
+class Login extends React.Component{
+    state={
+        email:'',
+        pwd:''
+    }
+    
+    handleChange= (e)=> {
+        const{name,value} = e.target
+        this.setState({[name]:value})
+    }
+    
+    handleSubmit= (e)=> {
+        e.preventDefault()
+    }
+
+
+    render(){
+       return(
+           <div>
+               <div>
+               <form onSubmit = {this.handleSubmit}>
+                    <input type='email' name='email' placeholder='email...' required onChange={this.handleChange}/>
+                    <input type='password' name='pwd' placeholder='password...' required onChange={this.handleChange}/>
+                    <button onSubmit= {this.handleSubmit}>Log in</button>
+               </form>
+               </div>
+           </div>
+       )
+    }
+}
+
+export default Login;
