4060b58c71f6c48e388648e705021ad556bdd924
Maikel de Werd <maikeldewerd@hotmail.com>
2021-12-10 11:00:42 +0100

    eindresultaat

--- netnix/src/Components/Segments/Segments.jsx
+++ netnix/src/Components/Segments/Segments.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import styles from './Segments.module.css'
 import placeholder from './assets2/place.jpeg'
+import placeholder1 from './assets2/Pasific.jpeg'
+import placeholder2 from './assets2/Tomb.jpeg'
+import placeholder3 from './assets2/Hacksaw.jpeg'
+import Popup from '../Popup'
+import {useState} from 'react'
 
 const Segments = ({title}) => {
+    const [buttonPopup, setButtonPopup] = useState(false);
     return (
         <div className={styles.films}>
+
+            
             <p className={styles.title}>{title}</p>
+            <main>
+                <button className={styles.btnPos} onClick={() => setButtonPopup(true)}>Open</button>
+            </main>
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup} >
+                <h3>Pacific Rim Uprising</h3>
+                <div className={styles.popContent}>
+                    <video className={styles.video} src="./assets2/trailer.mp4"></video>
+                    <p>
+                        Jake, de zoon va Stacker Pentecost, gaat samen met een oude pilotenvriend en een Jaeger-hacker opnieuw de strijd aan met Kaiju-monsters om de mensheid te redden.
+                    </p> 
+                </div>    
+            </Popup>
             <div className={styles.row}>
-                <img className={styles.image} src={placeholder} alt="" />
-                <img className={styles.image} src={placeholder} alt="" />
-                <img className={styles.image} src={placeholder} alt="" />
+                <img className={styles.image} src={placeholder1} alt="" />
+                <img className={styles.image} src={placeholder2} alt="" />
+                <img className={styles.image} src={placeholder3} alt="" />
                 <img className={styles.image} src={placeholder} alt="" />
                 <img className={styles.image} src={placeholder} alt="" />
             </div>
