2d713b076b3eea6ddec8abcf2b31665b7b30dde0
Maikel de Werd <maikeldewerd@hotmail.com>
2021-12-10 08:58:38 +0100

    New logo

--- netnix/src/Components/Navigation/Navigation.jsx
+++ netnix/src/Components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Navigation.module.css'
-import logo from './assets/logo.svg'
+import logo from './assets/netnix.png'
 import SecondaryNav from '../SecondaryNav'
 
 const Navigation = () => {
